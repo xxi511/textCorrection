a298415eb6739ffd0ddef475de3af5a9b5903f21
xxi511 <s941314g@yahoo.com.tw>
2016-01-15 12:48:28 +0800

    replace star word and help dicide line

--- change/change/Form1.cs
+++ change/change/Form1.cs
@@ -20,7 +20,7 @@ namespace change
 
         private void startBtn_Click(object sender, EventArgs e)
         {
-#region Get Data Detail
+            #region Get Data Detail
             string dataFile = @"data.txt";
             StreamReader data = new StreamReader(dataFile, System.Text.Encoding.Default); //讀取data
 
@@ -28,48 +28,32 @@ namespace change
 
             List<string> oldWord = new List<string>();
             List<string> newWord = new List<string>();
-            List<string> exOldWord = new List<string>();
-            List<string> exNewWord = new List<string>();
-            bool isEx = false;
-            
 
             while (data.Peek() != -1)
             {
                 string currentRow = data.ReadLine();
                 string[] tempword = currentRow.Trim().Split(' '); // 將字串以空白字元分解，第一個是舊的第二個是新的
-                
+
                 foreach (string w in tempword)
                 {
                     if (!(w.Trim() == "" || w.Trim() == null))
                     {
-                        if (w != "**" && !isEx)
-                        {
-                            if (line % 2 == 0)
-                                oldWord.Add(w); // 將字串存入 List
-                            else
-                                newWord.Add(w);
-                        }
+                        if (line % 2 == 0)
+                            oldWord.Add(w); // 將字串存入 List
                         else
-                        {
-                            isEx = true;
-                            if(w == "**")
-                                continue;
-                            else if (line % 2 == 0)
-                                exOldWord.Add(w); // 將字串存入 List
-                            else
-                                exNewWord.Add(w);
-                        }
+                            newWord.Add(w);
+
                         line += 1;
                     }
                 }
             }
             data.Close();
-#endregion
-#region 第一次修改
+            #endregion
+            
             // 拆成一行一行丟進去，willChange[0]表示第一行，[1]表示第二行...
-            string [] willChange = (!String.IsNullOrEmpty(novelText.Text.Trim())) ? novelText.Lines : null; 
+            string[] willChange = (!String.IsNullOrEmpty(novelText.Text.Trim())) ? novelText.Lines : null;
             novelText.Text = ""; //處理完過的文字會全部塞在裡面
-            for(int i=0;i<willChange.Length;i++)
+            for (int i = 0; i < willChange.Length; i++)
             {
                 #region 最前面替換或插入兩個全形空白
                 char[] b = new char[willChange[i].Length];
@@ -78,13 +62,13 @@ namespace change
                     sr.Read(b, 0, willChange[i].Length);
                     for (int index = 0; index < b.Length; index++)
                     {
-                        if ( !String.Equals(b[index], ' ') && !String.Equals(b[index], '　'))
+                        if (!String.Equals(b[index], ' ') && !String.Equals(b[index], '　'))
                         {
                             //不是半形空白或全形空白
                             var tempStr = new StringBuilder(willChange[i]);
                             int deleteLength = (index == 0) ? 0 : index;
                             tempStr.Remove(0, deleteLength);
-                            tempStr.Insert(0,"　　");
+                            tempStr.Insert(0, "　　");
                             willChange[i] = tempStr.ToString();
                             break;
                         }
@@ -100,24 +84,24 @@ namespace change
                     willChange[i] = cor;
                 }
             }
-#endregion
-#region 第二次修改
-            for (int i = 0; i < willChange.Length; i++)
-            {
-                string cor = "";
-                for (int j = 0; j < exOldWord.Count; j++)
-                {
-                    cor = willChange[i].Replace(exOldWord[j], exNewWord[j]);
-                    willChange[i] = cor;
-                }
-            }
-#endregion
+       
 
             for (int i = 0; i < willChange.Length; i++)
             {
                 novelText.Text += willChange[i];
                 novelText.Text += "\n";
+                if (i > 0 && 
+                    !String.Equals(willChange[i - 1], "") &&
+                    !String.Equals(willChange[i],""))
+                {
+                    //willChange是一行一行的字
+                    //如果上面那行不是空白，就是沒分段的意思
+                    //加一行空白
+                    //如果自己就是空白了，那就不用加了
+                    novelText.Text += "\n";
+                }
+                
             }
-        } 
+        }
     }
 }
